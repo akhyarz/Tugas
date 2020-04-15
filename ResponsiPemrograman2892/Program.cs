a08b47be641ee6a0e16a5a4116a24caff9d13d78
akhyarz <61868811+akhyarz@users.noreply.github.com>
2020-04-15 15:37:45 +0700

    Add files via upload

---
+++ ResponsiPemrograman2892/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ResponsiPemrograman2892
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            karyawan karyawan = new karyawan();
+
+            karyawan.NIK1 = "1903022123";
+            karyawan.NAMA1 = "bima";
+            karyawan.GAJI1 = 2500000;
+
+            karyawan.GAJIBULANAN();
+            karyawan.kenaikangaji();
+
+
+
+        }
+    }
+}
---
+++ ResponsiPemrograman2892/karyawan.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ResponsiPemrograman2892
+{
+    class karyawan
+    {
+        public string NAMA1;
+        public string NIK1;
+        public float GAJI1;
+        public string NAMA2;
+        public string NIK2;
+        public float GAJI2;
+
+        public void GAJIBULANAN()
+        {
+            Console.WriteLine("NO NIK/ NAMA \t\t\tGAJIBULANAN");
+            Console.WriteLine("==============================");
+            Console.WriteLine("1.{0} {1}\t\t{2}", NIK1, NAMA1, GAJI1);
+            Console.WriteLine("1.{0} {1}\t\t{2}", NIK2, NAMA2, GAJI2);
+            Console.WriteLine("   ");
+        }
+        public void kenaikangaji ()
+        {
+            Console.WriteLine(" ");
+            Console.WriteLine("Asyiiiiik kenaikana gaji 10%!! ;"
+            Console.WriteLine("==============================");
+            
+           
+        }
+    }
+
+    }
+}
